6221b476776d9b7c8f2839eb114a38f708376625
SantyJR18 <manzanares.santiago@hotmail.com>
2023-04-26 16:01:11 -0600

    bd crud

---
+++ DBPrueba26.sql
@@ -0,0 +1,30 @@
+--DDL
+/*Crear bd*/
+
+create database DBPrueba26;
+go
+
+use DBPrueba26
+go
+
+create table Ciudad (
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	nombre VARCHAR(30) NOT NULL,
+	estado BIT DEFAULT 'true'
+);
+go
+
+CREATE TABLE Persona (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    primerNombre VARCHAR(50) NOT NULL,
+	segundoNomnre VARCHAR(50) NULL,
+    primerApellido VARCHAR(50) NOT NULL,
+	segundoApellido VARCHAR(50) NULL,
+    fecha_nacimiento DATETIME NOT NULL,
+    sexo BIT DEFAULT 1 NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    telefono VARCHAR(20),
+    direccion VARCHAR(100),
+	estado BIT DEFAULT 'true'
+);
+go
